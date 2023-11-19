6306c71466e469540b5208c575faec6ba6cde8ab
Tuli <53302078+tuli-pen@users.noreply.github.com>
2023-11-19 12:29:49 -0500

    Update NodeFight.py

--- modelView/nodes/NodeFight.py
+++ modelView/nodes/NodeFight.py
@@ -6,14 +6,9 @@ class NodeFight(Node) :
         super().__init__()
         self.type = NodeType.FIGHT
         self.oponent = 0x000
-        self.options = []
 
     def getOponent(self):
         return self.oponent
-    def getOptions(self):
-        return self.options
 
     def setOponent(self, oponent):
         self.oponent = oponent
-    def setOptions(self, options):
-        self.options = options
