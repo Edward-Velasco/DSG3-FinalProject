546804d9a8d541ec5784b6247afc75f2786cdb6a
Pablo Luna Guzmán <yareaj@proton.me>
2023-11-23 00:57:37 -0500

    feat: display_story_node method for GameInterface

--- view/main.py
+++ view/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+
 # Create the built Classes
 from view.classes.Button import Button
 from view.classes.TextOnlyBox import TextOnlyBox
@@ -76,6 +77,53 @@ class GameInterface:
                         # When the last intro image is reached, move to whatever is next
                         return
                     cutscenes_iterator += 1
+
+            # Show the changes
+            pygame.display.flip()
+
+    def display_story_node(self, story_node):
+        self.set_background("view/assets/backgrounds/nodes_bg.png")
+
+        story_text_box = TextOnlyBox(
+            background_image="view/assets/backgrounds/text_container_bg.png",
+            center_coordinates_pair=[292+(964/2), 316+(373/2)],
+            text_input=story_node["content"],
+            text_color="Black",
+            font=get_font(30)
+        )
+
+        left_choice_button = Button(
+            background_image="view/assets/buttons/choice_left_button.png",
+            center_coordinates_pair=[292+35+(434/2), 316+258+(85/2)],
+            text_input=story_node["options"][0],
+            text_color="White",
+            font=get_font(50),
+            text_hovering_color="Gray",
+            uuid="PLACEHOLDER"
+        )
+
+        right_choice_button = Button(
+            background_image="view/assets/buttons/choice_right_button.png",
+            center_coordinates_pair=[292+490+(434/2), 316+258+(85/2)],
+            text_input=story_node["options"][1],
+            text_color="White",
+            font=get_font(50),
+            text_hovering_color="Gray",
+            uuid="PLACEHOLDER"
+        )
+
+        # TODO: UUID system for button class :D
+
+        while True:
+            story_text_box.multiline_text_render(self.screen, 0)
+            left_choice_button.display_button_update(self.screen)
+            right_choice_button.display_button_update(self.screen)
+
+            # Check for user inputs
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
             # Show the changes
             pygame.display.flip()
