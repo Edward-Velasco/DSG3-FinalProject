dbad710bdf686a1346a6fb9d0f7b696f32dacc1c
Pablo Luna Guzmán <yareaj@proton.me>
2023-11-23 00:58:02 -0500

    test: Display testing for the story mode

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-# from view.main import start_gui
+"""
 from modelView.GUI import GUI
 from modelView.Deadpool import Deadpool
 
 # disjointSets.build()
 gui = GUI()
 deadpool = Deadpool(gui)
-gui.start(deadpool)
+gui.start(deadpool)"""
 
+from view.main import GameInterface
+from model.StoryTree import StoryTree
+
+GameScreen = GameInterface()
+#GameScreen.start_gui()
+
+GameScreen.display_story_node(StoryTree[1])
