2c7bb4129a61e0a70292f9834711aa376fccb4d9
Edward_Velasco <evelascon@unal.edu.co>
2023-11-26 13:14:13 -0500

    docs: Characters.py indications of examples that have to be used for current and final implementation

--- model/Characters.py
+++ model/Characters.py
@@ -1,21 +1,21 @@
 # Ejemplo de conjuntos disyuntos
 # Cada valor debe ser un objeto de tipo Character!
 
-from modelView.Types import Code
+from modelView.Types import Code, NodeType
 from modelView.nodes.NodeSimple import NodeSimple
 
 Characters = {
-
-    0: Character("Ironman", "/", "/", "/", NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101])),
-    
-    # 0: {
-    #     'name': "Ironman",
-    #     'pictureRoute': "/",
-    #     'miniatureRoute': "/",
-    #     'miniatureDeadRoute': "/",
-    #     'fightContent': NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101]),
-    #     'state': True
-    # },
-
+    # Use this example when Character class is implemented
+    # 0: Character("Ironman", "/", "/", "/", NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101])),
+ 
+    # Example for current implementation
+    0: {
+        'name': "Ironman",
+        'pictureRoute': "/",
+        'miniatureRoute': "/",
+        'miniatureDeadRoute': "/",
+        'fightContent': NodeSimple(NodeType.STORY, "Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101]),
+        'state': True
+    },
 }
 
