dddb40788f4e0c74970b4e373ac54a2af97789da
Edward_Velasco <evelascon@unal.edu.co>
2023-11-26 11:48:07 -0500

    example: Characters.py example updated (still needs refactor)

--- model/Characters.py
+++ model/Characters.py
@@ -1,14 +1,16 @@
 # Ejemplo de conjuntos disyuntos
 # Cada valor debe ser un objeto de tipo Character!
 
+from modelView.Types import Code
+from modelView.nodes.NodeSimple import NodeSimple
+
 Characters = {
     0: {
         'name': "Ironman",
-        'fightContent': "Air battle",
-        'fightButtonText': "Rip mask",
         'pictureRoute': "/",
         'miniatureRoute': "/",
         'miniatureDeadRoute': "/",
+        'fightContent': NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101]),
         'state': True
     },
     1: {
