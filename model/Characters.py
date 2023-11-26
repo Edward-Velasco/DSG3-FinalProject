a4aaa060287849f1cc7b54252e0eddf274c7038c
Edward_Velasco <evelascon@unal.edu.co>
2023-11-26 13:04:29 -0500

    example: Characters.py example of final dictionary structure with Character class implemented

--- model/Characters.py
+++ model/Characters.py
@@ -5,58 +5,17 @@ from modelView.Types import Code
 from modelView.nodes.NodeSimple import NodeSimple
 
 Characters = {
-    0: {
-        'name': "Ironman",
-        'pictureRoute': "/",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'fightContent': NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101]),
-        'state': True
-    },
-    1: {
-        'name': "Captain America",
-        'fightContent': "That shield breaks the laws of physics!",
-        'fightButtonText': "Go reunite with your 838 gf",
-        'pictureRoute': "/",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'state': True
-    },
-    2: {
-        'name': "Hulk",
-        'fightContent': "Hulk smash!",
-        'fightButtonText': "Run, I can't against this one",
-        'pictureRoute': "/",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'state': True
-    },
-    3: {
-        'name': "Black widow",
-        'fightContent': "No powers? Seriously? How did you even get in the group?",
-        'fightButtonText': "Slashhhh",
-        'pictureRoute': "/",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'state': True
-    },
-    4: {
-        'name': "God Hawkeye",
-        'fightContent': "Finally, a worthy opponent, our battle will be legendary",
-        'fightButtonText': "I'm not Scarlet Witch, you can't use your mental tricks on me",
-        'pictureRoute': "/",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'state': True
-    },
-    5: {
-        'name': "Spider-man",
-        'fightContent': "Never forget the hyphen between Spider and man",
-        'fightButtonText': "C'mon spidey, don't make me do this, you are my best friend",
-        'pictureRoute': "/",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'state': True
-    }
+
+    0: Character("Ironman", "/", "/", "/", NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101])),
+    
+    # 0: {
+    #     'name': "Ironman",
+    #     'pictureRoute': "/",
+    #     'miniatureRoute': "/",
+    #     'miniatureDeadRoute': "/",
+    #     'fightContent': NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101]),
+    #     'state': True
+    # },
+
 }
 
