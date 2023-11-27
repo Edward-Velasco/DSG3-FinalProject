dce36911b0f6ded23857734454ee5d40e996b8ff
Edward_Velasco <evelascon@unal.edu.co>
2023-11-27 00:22:27 -0500

    example: Characters.py final implementation example

--- model/Characters.py
+++ model/Characters.py
@@ -2,20 +2,29 @@
 # Cada valor debe ser un objeto de tipo Character!
 
 from modelView.Types import Code, NodeType
+from modelView.Character import Character
 from modelView.nodes.NodeSimple import NodeSimple
 
 Characters = {
-    # Use this example when Character class is implemented
-    # 0: Character("Ironman", "view/assets/characters/ironman.png", "/", "/", NodeSimple("Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101])),
- 
-    # Example for current implementation
-    0: {
-        'name': "Ironman",
-        'pictureRoute': "view/assets/characters/ironman.png",
-        'miniatureRoute': "/",
-        'miniatureDeadRoute': "/",
-        'fightContent': NodeSimple(NodeType.STORY, "Air battle", ["Stab the head", "Rip off a leg"], [Code.DKTMU, 101]),
-        'state': True
-    },
+    0: Character("Ironman",
+                 "view/assets/characters/ironman.png",
+                 "/",
+                 "/",
+                 NodeSimple(
+                    NodeType.STORY,
+                    "Air battle",
+                    ["Stab the head", "Rip off a leg"],
+                    [Code.DKTMU, 101])
+                 ),
+    1: Character("Hulk",
+                 "/",
+                 "/",
+                 "/",
+                 NodeSimple(
+                    NodeType.STORY,
+                    "Hulk smash",
+                    ["Call spidey", "Pray for it not to be painful"],
+                    [101, 101])
+                 ),
 }
 
