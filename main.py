7ead12be2ba24f1b5a3dafff24ae463f1e3e7aba
Edward_Velasco <evelascon@unal.edu.co>
2023-11-27 18:10:55 -0500

    refactor: main.py is now up to date with definitive implementation

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-from modelView.Deadpool import Deadpool
-# from modelView.DisjointSets import DisjointSets
 from view.main import GameInterface
-from view.test import Root_test_tree, Dialogue_test_node, Fight_test_node
+from modelView.DisjointSets import Disjoint_Set as DisjointSets
+from modelView.Deadpool import Deadpool
 
-# sets = DisjointSets()
-# sets.build()
-
-sets = None
-
+sets = DisjointSets(100)
+sets.build()
 GameScreen = GameInterface()
 deadpool = Deadpool(GameScreen, sets)
 GameScreen.set_deadpool(deadpool)
 GameScreen.start_gui()
-#GameScreen.display_story_node(Root_test_tree)
-#GameScreen.display_character_node(Fight_test_node)
-
-
-# How main should look
-"""
-from view.main import GameInterface
-from modelView.DisjointSets import DisjointSets
-from modelView.Deadpool import Deadpool
-
-sets = DisjointSets()
-sets.build()
-GameScreen = GameInterface()
-deadpool = Deadpool(GameScreen, sets)
-GameScreen.start_gui(deadpool)
-sets.importDeadpool(deadpool)
-"""
+# sets.set_deadpool(deadpool)
 
