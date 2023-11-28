f11162522511c8d8f8f85b2c69ec21a3e86f6718
Edward_Velasco <evelascon@unal.edu.co>
2023-11-28 03:44:46 -0500

    fix: main.py order of inicialization

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from view.main import GameInterface
 from modelView.DisjointSets import Disjoint_Set as DisjointSets
 from modelView.Deadpool import Deadpool
 
-sets = DisjointSets(100)
+sets = DisjointSets(4)
 sets.build()
 GameScreen = GameInterface()
 deadpool = Deadpool(GameScreen, sets)
 GameScreen.set_deadpool(deadpool)
+sets.setDeadPool(deadpool)
 GameScreen.start_gui()
-# sets.set_deadpool(deadpool)
 
