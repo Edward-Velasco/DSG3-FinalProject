d1cc6ae66da16b7455dc598f7f22ed2f4bcff062
Edward_Velasco <evelascon@unal.edu.co>
2023-11-28 01:15:10 -0500

    style: ConsoleColor enum class temporarily added to ease debugging

--- modelView/Types.py
+++ modelView/Types.py
@@ -24,3 +24,12 @@ class InfinityStones(Enum):
     RED = "red"
     ORANGE = "orange"
 
+class ConsoleColor:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
